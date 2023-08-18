b16e1c431bca8524293b3f8fb137154acb239522
JoeChang <josephachang02@gmail.com>
2023-08-18 12:09:59 -0700

    finished my part

---
+++ index.js
@@ -0,0 +1,55 @@
+A. Q + A:
+    1. The only way assign a variable is through the equal operator (=). This allows us to assign a value to the given variable.ex let x= 10. This assigns the variable with the value of 10
+    2. We can change the value of a variable by using the (=) operator with a const declaration so that the value of the variable is the same throughout the script.
+    3. With the existing variable, you can use the = operator to have the new variable equal to the old one. Ex. Let x = 10; let x = newVariable;
+    4.  Declare:
+            This is to call out and create a new variable, it doesn't necessarily need to include a value. The declared variable can also be called back.
+        Assign:
+            This allows us to give the variable that we are working with a variation of data types such as; number, string, boolean, object, array, null, undefined, symbol, function
+        Define: Reassigning the value that we can later callback and assign in the code.
+    5. Pseudo coding is how we can write our code prior to writing the actual lines into the program. This allows us to write out our intentions for what we want the operator and assignments to do. It allows us to break down each step of what will happen in our program.
+    6. Before we even begin writing our code, we should constantly be thinking about what is being asked, what operators are needed to proceed with the steps. This allows us to break down clearly what steps we need to take. Working smarter not harder.
+B. Strings:
+    let firstVariable =;
+    let firstVariable ="Hello World";
+    let firstVariable = "20";
+    let secondVariable = firstVariable;
+    let secondVariable = "Goodbye World";
+    firstVariable = "Goodbye World";
+    let myName = "Joe Chang"
+    console.log("Hello, my name is (myName)");
+C. Booleans: ALL PRINT TRUE
+    console.log(a != b)
+    console.log(a != d);
+    console.log('Kevin' === 'Kevin');
+    console.log(true || false);
+    console.log(false || false || false || false || false || true);
+    console.log(e === 'Kevin');
+    console.log(a <= b <=c);
+    console.log(a <= b < d);
+    console.log(48 == '48');
+D. The Farm:
+    let animal = "";
+    if (animal === "cow"){
+    console.log("mooooo");
+    } else {
+    console.log("Hey! You're not a cow.");
+    }
+E. Driver's Ed:
+    let age= "19"
+    if (age >=16){
+    console.log("Here are the keys!");
+    } else {
+    console.log("Sorry, you're too young.");
+    }
+
+
+
+
+
+
+
+
+
+
+
